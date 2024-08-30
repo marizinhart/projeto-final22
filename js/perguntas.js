0349a7e3485895816b06eeb4dd16923713d20322
Mari Moreira <107552602+marizinhart@users.noreply.github.com>
2024-08-30 08:24:20 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,161 @@
+export const perguntas = [
+    {
+        enunciado: "Apos você sair da escola, tu se depara com uma tecnologia nova, isto é:",
+        alternativas: [
+            {
+                texto: "Surpriendente!",
+                afirmacao: [
+                "A tecnologia veio para facilitar nossa vida,",
+               "Como uma forma de melhoração,"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "horrível!",
+                afirmacao:[ "A tecnlogia é um problema nos dias de hoje,",
+                "Não vai facilitar a vida de ninguém"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "O que é uma ingteligencia artificial?",
+        alternativas: [
+            {
+                texto:  "É a capacidade que uma máquina tem para produzir competências semelhantes ás humanas como raciocínio!",
+                afirmacao: [ "nos auxiliando em tudo que precisamos,",
+                  "vai nos proporcionar melhoramento,"
+                ],
+                proxima: 3,
+            },
+            {
+                texto:  "É uma capacidade que não trás benefício nenhum",
+                afirmacao: [ "ela nos tira a concentração,",
+                   "não nos deixa ficar focados no que vamos fazer,"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado:  "Onde é usada essa tecnologia?",
+        alternativas: [
+            {
+                texto: "Em sistemas de reconhecimento de imagem, como placas e etc!",
+
+                afirmacao: [ "alem de ser uma forma mais pratica e facil de se conectar.",
+                 "sera muito mais facil de se conectar,"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não é usada em nada!",
+                afirmacao: [  "nos causam problemas na saude, entre outros.",
+                 "vai nos trazer malefícios para nosso bem estar,"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "O que ela pode fazer?",
+        alternativas: [
+            {
+                texto: "Expressar emoções, empatia e ter sentimentos!",
+                afirmacao: [ "Não vai trazer nenhum beneficio.",
+                "não teremos boa comunicação."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Avaliar dados, analisar imagens, ajudar em trabalhos!",
+                afirmacao:  [ "Resumindo a tecnologia veio para facilitar nossa vida.",
+                "além de nos auxiliar nos estudos."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Como ela pode mudar a maneira como trabalhamos?",
+        alternativas: [
+            {
+                texto: "Ela não interfere em nada!",
+                afirmacao: [ "Resumindo a tecnologia veio para atrasar nossa vida.",
+                "A tecnologia veio nos desconectar do mundo real."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Nos ajudando a fazer um trabalho, orgânizando nossos horarios!",
+                afirmacao: [ "A tecnologia é um grande melhoramento de vida.",
+                "Portanto a tecnologia é uma ferramenta estremamente utíl."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "O que ela defende?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos para os humanos.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e de melhoria"
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "As pessoas perderão seus empregos para máquinas.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a querer cancelar o AI",
+                    "Busca reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro",
+        alternativas: [
+            {
+                texto: "Podem ter novas oportunidades de emprego.",
+                afirmacao: [
+                    "Vem para auxiliar nos trabalhos.",
+                    "Serve para achar soluções."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Vai acabr com o trabaljo.",
+                afirmacao: [
+                    "Sua preocupação é que essa tecnologia impulsione a ninguém querer mais trabalhar.",
+                    "As pessoas desacreditar do trabalho."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É facil lidar com a AI?",
+        alternativas: [
+            {
+                texto: "Não é tão facil para algumas pessoas",
+                afirmacao: [
+                    "Não é tão facil mas pode te auxiliar muito",
+                    "Com algumas práticas tudo vai fazendo sentido."
+                ],
+            },
+            {
+                texto: "É um terror usar a AI",
+                afirmacao: [
+                    "Para você aprender demora muito e não intende nada!",
+                    "Nem com práticas você ira aprender, pos é muito complicado,",
+                    "Essa nova modalidade se torna um completo fracasso."
+                ],
+            },
+        ]
+    }
+];
